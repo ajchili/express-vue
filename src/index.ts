570350b2d345d10a4ad32f8f76e63ecd0c138188
Kirin Patel <kirinpatel@gmail.com>
2020-08-20 22:49:10 -0400

    Setup ejs and Vue.js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Response, Request } from 'express';
 
 const app = express();
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-app.get('*', (_: Request, res: Response) => {
-  res.status(200).send('Hello, World!');
+app.get('/', (_: Request, res: Response) => {
+  res.render('index.ejs');
 });
 
 app.listen(8080);
