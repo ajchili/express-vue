d0fb256762619f4d4d7698b71ba0a1635a5eba21
Kirin Patel <kirinpatel@gmail.com>
2020-08-20 22:56:49 -0400

    Setup extremely simple SSR

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', (_: Request, res: Response) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    message: 'I was rendered by express & ejs!',
+  });
 });
 
 app.listen(8080);
